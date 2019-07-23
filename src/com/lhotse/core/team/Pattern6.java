83c3e0442043763c2f60ead29ee020c3b5eb32b5
Shivam Kumar Chauhan <shivamkumarchauhan.com@gmail.com>
2019-07-23 12:44:06 +0530

    ok

--- src/com/lhotse/core/team/Pattern6.java
+++ src/com/lhotse/core/team/Pattern6.java
@@ -5,9 +5,9 @@ public class Pattern6 {
         int n = 0;
         for(int i = 1;i <=5;i++){
             for(int j = 1; j<=i;j++) {
-                System.out.print(j + n);
+                n++;
+                System.out.print(n+ " ");
             }
-            n++;
             System.out.println(" ");
         }
     }
