0ec59a53563f25178629e86e0bd802d16f87d4b0
Ankit Chauhan <advik.ankitchauhan85@gmail.com>
2019-07-23 19:36:45 +0530

    commit after revierting changes from homepage code.

--- src/com/lhotse/core/team/HelloWorld.java
+++ src/com/lhotse/core/team/HelloWorld.java
@@ -3,6 +3,6 @@ package com.lhotse.core.team;
 public class HelloWorld {
 
     public static void main(String... arg){
-        System.out.println("Hello Ankur");
+        System.out.println(" Hello Ankur");
     }
 }
