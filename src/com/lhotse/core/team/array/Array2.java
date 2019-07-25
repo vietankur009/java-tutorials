1307f2a9d27c2ac2471aa1a5ba85b053bc5e5fed
Shivam Kumar Chauhan <shivamkumarchauhan.com@gmail.com>
2019-07-25 13:34:06 +0530

    someting new

--- src/com/lhotse/core/team/array/Array2.java
+++ src/com/lhotse/core/team/array/Array2.java
@@ -5,18 +5,22 @@ package com.lhotse.core.team.array;
  */
 public class Array2 {
     public static void main(String[] args) {
-        ar2();
+        reverse();
     }
 
-    public static void ar2() {
-        int[] i = new int[5];
-        i[0] = 40;
-        i[1] = 70;
-        i[2] = 10;
-        i[3] = 50;
-        i[4] = 20;
+    public static void reverse() {
+        int[] i = new int[]{40,70,10,50,20};
+        int[] ar = new int[5];
+
+        int n =0;
         for (int j = 4; j < i.length && j>=0; j--) {
-            System.out.print(i[j] + " ");
+            ar[n] = i[j];
+            n++;
+        }
+
+
+        for (int t=0; t<ar.length ; t++){
+            System.out.print(ar[t]+ " ");
         }
     }
 }
