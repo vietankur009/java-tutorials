87e2d63789ac5394049090f23016c02e6827f705
Shivam Kumar Chauhan <shivamkumarchauhan.com@gmail.com>
2019-07-27 12:09:40 +0530

    again

--- src/com/lhotse/core/team/array/Array6.java
+++ src/com/lhotse/core/team/array/Array6.java
@@ -1,4 +1,22 @@
 package com.lhotse.core.team.array;
 
 public class Array6 {
+    public static void main(String[] args) {
+        int[] i = new int[]{91, 70, 110, 50, 20};
+        int smallest = i[0];
+        int index = 0;
+        for (int j = 1; j < i.length; j++) {
+            if (i[j] < smallest) {
+                smallest = i[j];
+                int temp = i[index];
+                i[index] = smallest;
+                i[j] = temp;
+            }
+            index++;
+        }
+        //System.out.print(smallest);
+        for (int r = 0; r < i.length; r++) {
+            System.out.print(i[r] + " ");
+        }
+    }
 }
