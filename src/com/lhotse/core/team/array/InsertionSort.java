ec3614ca695dd111e1ee5b146332b34824b2261e
Shivam Kumar Chauhan <shivamkumarchauhan.com@gmail.com>
2019-07-29 12:30:54 +0530

    not

--- src/com/lhotse/core/team/array/InsertionSort.java
+++ src/com/lhotse/core/team/array/InsertionSort.java
@@ -1,5 +1,19 @@
 package com.lhotse.core.team.array;
 
 public class InsertionSort {
-
+    public static void main(String[] args) {
+        int[] ar = new int[]{4, 3, 2, 10, 12, 1, 5, 6};
+        for (int m = 0; m < ar.length-1; m++) {
+            int index  = m;
+            if(ar[m+1] < ar[m]){
+                index = m+1;
+            }
+            int a = ar[index];
+            for(int j = index-1;j>=0;j--){
+                ar[j+1] = ar[j];
+            }
+            ar[m] = a;
+            System.out.print(ar[m] + " ");
+        }
+    }
 }
