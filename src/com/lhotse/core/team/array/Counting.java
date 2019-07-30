da96ac703549d68f4a5c751fcfb4fbf2255a413c
Shivam Kumar Chauhan <shivamkumarchauhan.com@gmail.com>
2019-07-30 21:49:10 +0530

    not new

--- src/com/lhotse/core/team/array/Counting.java
+++ src/com/lhotse/core/team/array/Counting.java
@@ -1,22 +1,22 @@
 package com.lhotse.core.team.array;
 
 /**
- * This program will print the counting of an element .
+ * This program will count the different element .
  */
 public class Counting {
     public static void main(String[] arg) {
         int[] a = {1, 2, 1, 3, 1, 5, 2, 3,5};
-        for (int i = 0; i < a.length; i++) {
+        for (int i = 0; i < a.length ; i++) {
             if (a[i] != -1){
                 int e = a[i];
                 int count = 1;
-                for (int j = i+1;j<a.length;j++){
-                    if(a[j] != -1){
-                        if(a[i] == a[j]){
+                for (int j = i+1;j<a.length ;j++){
+                    //if(a[j] != -1){
+                        if(a[i] == a[j] && a[j] != -1){
                             count = count +1;
                             a[j] = -1;
                         }
-                    }
+                    //}
                 }
                 System.out.println("element" +" "+ e +" " +"value"+" " + count);
             }
--- src/com/lhotse/core/team/array/InsertionSort.java
+++ src/com/lhotse/core/team/array/InsertionSort.java
@@ -1,31 +1,29 @@
 package com.lhotse.core.team.array;
 
 /**
- * This program will sort array using insertion algorithm.
+ * This program will sort array using insertion sort algorithm.
  */
 public class InsertionSort {
     public static void main(String[] args) {
         int[] ar = {4, 3, 2, 10, 12, 1, 5, 6};
-        for (int m = 0; m < ar.length; m++) {
-            int index = m;
-            int a = ar[m];
-            for (int j = m - 1; j >= 0; j--) {
-                if (a < ar[j]) {
-                    ar[m] = ar[j];
-                    index = j;
-                    ar[index] = a;
-                }else {
+        for(int i=0;i<ar.length;i++) {
+            int j = i;
+            for (j = i; j > 0; j--) {
+                if (ar[j - 1] > ar[j]) {
+                    int temp = ar[j - 1];
+                    ar[j - 1] = ar[j];
+                    ar[j] = temp;
+                } else {
                     break;
                 }
-                System.out.print(ar[index] + " ");
             }
-            //ar[m] = a;
-            System.out.print(ar[m] + " ");
         }
 
-        /*for (int k = 0; k < ar.length; k++) {
+        for (int k = 0; k < ar.length; k++) {
             System.out.print(ar[k] + " ");
-        }*/
+        }
+    }
+}
 
        /* for(int i=0;i<a.length;i++)
         {
@@ -51,21 +49,20 @@ public class InsertionSort {
             }
         }
 
-        for(int x : a)
-        {
-            System.out.print(x + " ");
-        }
 */
-
-        /*for (int j = m; j > 0; j--) {
-            if (ar[j - 1] > ar[j]) {
-                int temp = ar[j - 1];
-                ar[j - 1] = ar[j];
-                ar[j] = temp;
-            } else {
-                break;
+      /* for (int m = 0; m < ar.length; m++) {
+        int index = m;
+        int temp = ar[m];
+            for (int j = m - 1; j >= 0; j--) {
+                if (temp < ar[j]) {
+                    ar[index] = ar[j];
+                    index = j;
+                } else {
+                    break;
+                }
+            ar[index] = temp;
             }
-        }*/
-    }
-}
+        }
+
+ */
 
