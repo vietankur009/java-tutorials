2b6e6a2e148cc789895132889edece31d6348f36
Shivam Kumar Chauhan <shivamkumarchauhan.com@gmail.com>
2019-08-13 16:35:57 +0530

    good

--- src/com/lhotse/core/team/programs/Pin.java
+++ src/com/lhotse/core/team/programs/Pin.java
@@ -1,12 +1,12 @@
 package com.lhotse.core.team.programs;
 
 public class Pin {
-    static int pinGenerate(int input1 , int input2, int input3){
-        int [] ar1 = new int[3];
-        int [] ar2 = new int[3];
-        int [] ar3 = new int[3];
+    static int pinGenerate(int input1, int input2, int input3) {
+        int ar1[] = new int[3];
+        int ar2[] = new int[3];
+        int ar3[] = new int[3];
         int i = 0;
-        while (input1 != 0 && input2!=0 && input3!=0) {
+        while (input1 != 0 && input2 != 0 && input3 != 0) {
             int re1 = input1 % 10;
             ar1[i] = re1;
             input1 = input1 / 10;
@@ -24,31 +24,56 @@ public class Pin {
         int sn1 = ar1[0];
         int sn2 = ar2[0];
         int sn3 = ar3[0];
-        for(int x = 1;x<ar1.length;x++){
-            if (ar1[x]<sn1){
+        int ln1 = ar1[0];
+        int ln2 = ar2[0];
+        int ln3 = ar3[0];
+        for (int x = 1; x < ar1.length; x++) {
+            if (ar1[x] < sn1) {
                 sn1 = ar1[x];
             }
 
-            if (ar2[x]<sn2){
+            if (ar2[x] < sn2) {
                 sn2 = ar2[x];
             }
 
-            if(ar3[x]<sn3){
+            if (ar3[x] < sn3) {
                 sn3 = ar3[x];
+            }
+
+            if (ar1[x] > ln1) {
+                ln1 = ar1[x];
+            }
+
+            if (ar2[x] > ln2) {
+                ln2 = ar2[x];
+            }
+
+            if (ar3[x] > ln3) {
+                ln3 = ar3[x];
             }
         }
 
-        //System.out.println(sn1);
-        int pin =0;
-        pin = pin*1 + sn3;
-        pin = pin*10+sn2;
-        pin = pin*10+sn1;
+        int large = 0;
+        if (ln1 > ln2 && ln1 > ln3) {
+            large = ln1;
+        } else if (ln2 > ln1 && ln2 > ln3) {
+            large = ln2;
+        } else {
+            large = ln3;
+        }
+
+        //System.out.println(ln1);
+        int pin = 0;
+        pin = pin * 1 + large;
+        pin = pin * 10 + sn3;
+        pin = pin * 10 + sn2;
+        pin = pin * 10 + sn1;
 
         return pin;
     }
 
-    public static void main(String[] arg){
-        int v = pinGenerate(123,345,234);
+    public static void main(String[] arg) {
+        int v = pinGenerate(462, 573, 310);
         System.out.print(v);
     }
 }
---
+++ src/com/lhotse/core/team/programs/SecWordUpperCase.java
@@ -0,0 +1,7 @@
+package com.lhotse.core.team.programs;
+
+public class SecWordUpperCase {
+    static void secUpperCase(String input1){
+        input1.split(" ");
+    }
+}
