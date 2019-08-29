e553d0254f67b4c389a051fa584cc07a2229ca15
Shivam Kumar Chauhan <shivamkumarchauhan.com@gmail.com>
2019-08-29 11:23:29 +0530

    how

--- src/com/lhotse/core/team/sprint/PojoDummyData.java
+++ src/com/lhotse/core/team/sprint/PojoDummyData.java
@@ -1,5 +1,43 @@
 package com.lhotse.core.team.sprint;
 
 public class PojoDummyData {
+    private int phone ;
+    private String name ;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    private String email ;
+    private String address ;
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
 
 }
